1b5eb381c22a7ccad3f8bce81af3b6610693848a
IanaFreya <freyayana@gmail.com>
2020-05-10 00:09:49 +0300

    add project
    add prj

---
+++ src/ToDo/toDoData.js
@@ -0,0 +1,17 @@
+const toDoData = [
+    {
+        id : 1,
+        text: "Закончить курсы по реакт",
+    },
+    {
+        id : 2, 
+        completed: true,
+        text: "Домашка по английскому",
+    },
+    {
+        id : 3,
+        text: "Сделать инстаграмм",
+    }
+]
+
+export default toDoData
